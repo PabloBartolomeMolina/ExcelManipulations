1354b9a4a69c57f380881422607e3acbd62f67dd
Pablo Bartolomé <pablobart.me@gmail.com>
2023-01-15 18:15:24 +0100

    Change location of CSV and Excel files. Update the path in code.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 from openpyxl import Workbook
 import time
 
-name = 'D:/airtravel.xlsx'
+name = 'D:/Python_Projects/ExcelManipulations/airtravel.xlsx'
 
 '''
 Function to read a CSV and create an Excel ".xlsx" file with the contents.
@@ -12,7 +12,7 @@ def csv_to_excel():
     wb = openpyxl.Workbook()
     ws = wb.active
     # CSV file can be downloaded from "https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html"
-    with open('D:/airtravel.csv') as f:
+    with open('D:/Python_Projects/ExcelManipulations/airtravel.csv') as f:
         reader = csv.reader(f, delimiter=',')
         for row in reader:
             ws.append(row)
