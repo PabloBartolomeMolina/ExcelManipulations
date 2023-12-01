ac107c8b4ba3e1d7338a207bcebc1839a0ba4ee7
Pablo Bartolomé <pablobart.me@gmail.com>
2023-12-01 23:37:44 +0100

    Minor change to uniform code

--- excelFileFormat.py
+++ excelFileFormat.py
@@ -73,7 +73,8 @@ def headers_direction(ws):
                 cell.font = Font(name='Arial', size=12, bold=True)
                 cell.fill = PatternFill(start_color=colorInitHeader, end_color=colorEndHeader, fill_type='solid')
     else:
-        return "inconclusive"
+        # Inconclusive result
+        pass
 
 
 # Function to format header's cells in each according to format of the worksheet.
