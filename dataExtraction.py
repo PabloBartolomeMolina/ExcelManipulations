95f9a578bce5a0d0766af6fc2de768631caf366f
Pablo Bartolomé <pablobart.me@gmail.com>
2023-12-01 23:23:50 +0100

    New files with data in horizontal

--- dataExtraction.py
+++ dataExtraction.py
@@ -1,6 +1,7 @@
 import csv, openpyxl
 
 csv_file = 'D:/Python_Projects/ExcelManipulations/airtravel.csv'
+csv_file2 = 'D:/Python_Projects/ExcelManipulations/airtravel_horizontal.csv'
 
 
 # Function to read a CSV and create an Excel ".xlsx" file with the contents.
@@ -10,6 +11,11 @@ def csv_to_excel(filename):
     ws.title = 'Raw data'
     # CSV file can be downloaded from "https://people.sc.fsu.edu/~jburkardt/data/csv/csv.html"
     with open(csv_file) as f:
+        reader = csv.reader(f, delimiter=',')   # Careful with the delimiter of your CSV.
+        for row in reader:
+            ws.append(row)
+    ws = wb.create_sheet("Horizontal_Data")
+    with open(csv_file2) as f:
         reader = csv.reader(f, delimiter=',')   # Careful with the delimiter of your CSV.
         for row in reader:
             ws.append(row)
