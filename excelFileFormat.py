bd6ae43d3d919128052b5823dd061dbc3c6ce1a0
Pablo Bartolomé <pablobart.me@gmail.com>
2023-12-01 23:15:07 +0100

    Make code more generic and easy to maintain

--- excelFileFormat.py
+++ excelFileFormat.py
@@ -1,21 +1,28 @@
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill, Font, GradientFill, Alignment
 
+# Define color for init and end for gradient coloring of headers cells.
+# They can be the same if only one color is wanted for this column / row with headers.
+colorInitHeader = '7CAAF0'
+colorEndHeader = '7CAAF0'
+
 
 # Open Workbook and modify it.
 def excel_rework(filename):
     wb = load_workbook(filename)
     ws = wb.active
     print('Contents:')
-    print(ws)
-    # Set the columns' width according to text of each column
-    for column_cells in ws.columns:
-        length = max(len(str(cell.value)) for cell in column_cells)
-        length = (length + 2) * 1.2
-        ws.column_dimensions[column_cells[0].column_letter].width = length
-
-    # Gradient color for one of the cells.
-    ws['B5'].fill = fill = GradientFill(stop=("FFFFFF", "000000"))
+    # Iterate through each worksheet.
+    for sheet_name in wb.sheetnames:
+        ws = wb[sheet_name]
+        print(ws)
+        # Set the columns' width according to text of each column
+        for column_cells in ws.columns:
+            length = max(len(str(cell.value)) for cell in column_cells)
+            length = (length + 2) * 1.2
+            ws.column_dimensions[column_cells[0].column_letter].width = length
+        # Gradient color for one of the cells.
+        ws['B5'].fill = fill = GradientFill(stop=("FFFFFF", "000000"))
 
     wb.save(filename)
 
@@ -31,7 +38,7 @@ def format_header_horizontal(filename):
         for row in ws.iter_rows(min_row=1, max_row=1, min_col=1, max_col=ws.max_column):
             for cell in row:
                 cell.font = Font(name='Arial', size=12, bold=True)
-                cell.fill = PatternFill(start_color='7CAAF0', end_color='7CAAF0', fill_type='solid')
+                cell.fill = PatternFill(start_color=colorInitHeader, end_color=colorEndHeader, fill_type='solid')
     wb.save(filename)
 
 
@@ -46,5 +53,5 @@ def format_header_vertical(filename):
         for row in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=1):
             for cell in row:
                 cell.font = Font(name='Arial', size=12, bold=True)
-                cell.fill = PatternFill(start_color='7CAAF0', end_color='7CAAF0', fill_type='solid')
+                cell.fill = PatternFill(start_color=colorInitHeader, end_color=colorEndHeader, fill_type='solid')
     wb.save(filename)
