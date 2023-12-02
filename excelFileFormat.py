2acba466f9bf5a4d11c853167831b006deb2eda6
Pablo Bartolomé <pablobart.me@gmail.com>
2023-12-02 22:05:20 +0100

    Improve headers for tables depending on orientation

--- excelFileFormat.py
+++ excelFileFormat.py
@@ -59,21 +59,21 @@ def format_header_vertical(filename):
 # Function to determine if the worksheet headers are horizontal or vertical.
 # Assumption is to have more data than headers.
 def headers_direction(ws):
-    if ws.max_row > ws.max_column:
+    if (ws.max_row > ws.max_column) or ("Vertical" in ws.title):
         # Apply the desired format 1st column containing headers.
         for row in ws.iter_rows(min_row=1, max_row=ws.max_row, min_col=1, max_col=1):
             for cell in row:
                 cell.font = Font(name='Arial', size=12, bold=True)
                 cell.fill = PatternFill(start_color=colorInitHeader, end_color=colorEndHeader, fill_type='solid')
-    elif ws.max_row < ws.max_column:
+    elif (ws.max_row < ws.max_column) or ("Horizontal" in ws.title):
         # Apply the desired format 1st row containing headers.
         for row in ws.iter_rows(min_row=1, max_row=1, min_col=1, max_col=ws.max_column):
             for cell in row:
                 cell.font = Font(name='Arial', size=12, bold=True)
                 cell.fill = PatternFill(start_color=colorInitHeader, end_color=colorEndHeader, fill_type='solid')
     else:
-        # Inconclusive result.
-        # Can be determined with the title of the worksheet.
+        # Inconclusive result. All cases should be already be covered in the previous cases, but in the case we have a
+        # squared matrix with headers in both vertical and horizontal axis, we can treat it here.
         pass
 
 
