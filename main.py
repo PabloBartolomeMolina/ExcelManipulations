c54477437095e3b3bacad38409f5c598cd2f61bd
Pablo Bartolomé <pablobart.me@gmail.com>
2023-12-29 20:03:02 +0100

    Call for the function that extract data from a VCF file. Pay attention to adapt filenames to your files

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import dataExtraction
 import excelFileFormat
+import vcfToEcxel
 
 excel_file = 'D:/Python_Projects/ExcelManipulations/airtravel.xlsx'
 
 
 # Main entry point.
 if __name__ == '__main__':
+    '''
     dataExtraction.csv_to_excel(excel_file)
     excelFileFormat.excel_rework(excel_file)
     excelFileFormat.format_header_vertical(excel_file)
+    '''
+    vcf_file_path = 'D:/FileWithContacts.vcf'
+    excel_file_path = 'D:/Ordered_Contacts.xlsx'
+    vcfToEcxel.vcf_to_excel(vcf_file_path, excel_file_path)
